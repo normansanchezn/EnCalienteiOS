840eb38becb7f8be848197ce273adef8da92fe35
norman.sanchez <normansanchezn@gmail.com>
2020-10-24 00:43:55 -0500

    [CREATE] Creando el proyecto en iOS

---
+++ EnCaliente/EnCaliente/EnCalienteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnCalienteApp.swift
+//  EnCaliente
+//
+//  Created by user on 24/10/20.
+//
+
+import SwiftUI
+
+@main
+struct EnCalienteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
